c55731c10437233113f728248c5724cd9689d481
GiovanniCrois <iticgiovannicruz@gmial.com>
2025-04-22 15:02:16 -0600

    changing gitignore

--- src/app/components/questionPanel.jsx
+++ src/app/components/questionPanel.jsx
@@ -8,7 +8,7 @@ export default function QuestionPanel() {
   };
 
   const [variant, setVariant] = useState(
-    "w-23 h-23 cursor-pointer bg-black text-white"
+    "font-Rammetto px-2 py-1 text-2xl font-extrabold text-pBlue text-center content-center cursor-pointer border-solid border-1 border-pBlue rounded-xl border-b-4 active:border-b-2"
   );
   const handleAnswer = (e) => {
     const value = e.target.value;
@@ -16,28 +16,38 @@ export default function QuestionPanel() {
     if (value == question.options[0]) {
       //setVariant("w-23 h-23 cursor-pointer bg-black text-white bg-green-500");
       e.target.className =
-        "w-23 h-23 cursor-pointer bg-black text-white bg-green-500";
+        "font-Rammetto px-2 py-1 text-2xl font-extrabold text-white bg-pBlue text-center content-center cursor-pointer border-solid border-1 border-pBlue rounded-xl border-b-4 active:border-b-2";
     } else {
       e.target.className =
-        "w-23 h-23 cursor-pointer bg-black text-white bg-red-500";
+        "font-Rammetto px-2 py-1 text-2xl font-extrabold text-white bg-red-500 text-center content-center cursor-pointer border-solid border-1 border-red-500 rounded-xl border-b-4 active:border-b-2";
     }
   };
 
   return (
     <>
-      <div>
-        {question.options.map((option) => {
-          return (
-            <button
-              value={option}
-              key={option}
-              onClick={handleAnswer}
-              className={variant}
-            >
-              {option}
-            </button>
-          );
-        })}
+      <div className="w-full">
+        <div className="w-full md:w-3/6 place-self-center grid grid-cols-1 mb-4">
+          <div className="border p-4  text-center text-2xl text-bold rounded-t-xl border-pBlue">
+            <h2 className="text-pBlue">Je</h2>
+          </div>
+          <div className="border p-4  text-center text-2xl text-bold rounded-b-xl border-pBlue">
+            <h2 className="text-pBlue">être</h2>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 place-self-center grid grid-cols-1 gap-2">
+          {question.options.map((option) => {
+            return (
+              <button
+                value={option}
+                key={option}
+                onClick={handleAnswer}
+                className={variant}
+              >
+                {option}
+              </button>
+            );
+          })}
+        </div>
       </div>
     </>
   );
--- src/app/verbsPractice/page.jsx
+++ src/app/verbsPractice/page.jsx
@@ -1,9 +1,13 @@
 import CountDownBar from "../components/countDownBar";
+import QuestionPanel from "../components/questionPanel";
 export default function verbsPractice() {
   return (
     <>
       <div className="mt-12 w-3/4 flex place-self-center">
-        <CountDownBar />
+        <div className="w-full grid grid-cols-1 content-center gap-8">
+          <CountDownBar />
+          <QuestionPanel />
+        </div>
       </div>
     </>
   );
