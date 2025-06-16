c5a1922ad87796c25c5fb0c2870aca0b4963740a
GiovanniCrois <iticgiovannicruz@gmial.com>
2025-06-16 15:40:29 -0600

    ui changes

--- src/app/normale/page.jsx
+++ src/app/normale/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import QuestionPanel from "../components/questionPanel";
-import { useState } from "react";
+import ResultPanel from "../components/resultsPanel";
+import { useState, useEffect } from "react";
 export default function () {
   const [success, setSuccess] = useState(0);
   const [attempts, setAttempts] = useState(3);
@@ -8,15 +9,26 @@ export default function () {
   const handleAttempts = (data) => {
     setAttempts(data);
   };
+
+  const handleSuccess = (data) => {
+    setSuccess(data);
+  };
   return (
     <>
       <div className="flex p-2">
         {Array.from({ length: attempts }).map((_, i) => (
-          <div className="rounded-full bg-red-500 size-15 flex"></div>
+          <div className="bg-red-500 size-15 [clip-path: polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%);]"></div>
         ))}
       </div>
 
-      <QuestionPanel setAttempts={handleAttempts} />
+      {attempts > 0 ? (
+        <QuestionPanel
+          setAttempts={handleAttempts}
+          setSuccess={handleSuccess}
+        />
+      ) : (
+        <ResultPanel aciertos={success} />
+      )}
     </>
   );
 }
