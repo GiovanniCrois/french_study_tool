f1da29874fdd9644cd16a46131842595fc95ec78
GiovanniCrois <iticgiovannicruz@gmial.com>
2025-07-07 14:55:27 -0600

    unsavedChanges

--- src/app/components/optionsDisplay.jsx
+++ src/app/components/optionsDisplay.jsx
@@ -2,13 +2,14 @@ import OptionButton from "./optionButton";
 export default function ({ options, handleAnswer, rightAnswer }) {
   return (
     <div className="w-full md:w-1/2 place-self-center grid grid-cols-1 gap-2">
-      {options.map((option) => {
+      {options.map((option, indx) => {
         if (option == rightAnswer) {
           return (
             <OptionButton
               option={option}
               onClick={handleAnswer}
               isCorrect={true}
+              indx={indx}
             />
           );
         } else {
